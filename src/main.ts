ebaa63a8c16bf2a2196df373432f66cc9ae9c626
anamkaj <anamkaj@gmail.com>
2024-05-18 20:32:29 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { config } from 'dotenv'
-import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
   app.setGlobalPrefix('api')
-  // app.useGlobalPipes(new ValidationPipe())
+
   config()
   app.enableCors({
     origin: '*',
