e2ab35e7f2c9361dbcd5dcfb6b7eac713cf052de
anamkaj <anamkaj@gmail.com>
2024-05-19 14:29:50 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.setGlobalPrefix('api')
   config()
+
+  app.enableCors({
+    origin: ['https://tmk-v.ru', 'http://localhost:3000'],
+    methods: 'GET,POST,OPTIONS,HEAD',
+    allowedHeaders:
+      'Authorization, Origin, X-Requested-With, Content-Type, Accept',
+  })
   await app.listen(5000)
 }
 bootstrap()
